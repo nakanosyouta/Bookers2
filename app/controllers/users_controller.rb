e76dd61a92606563c0b3607c8741637bfd1adf06
112259071+nakanosyouta@users.noreply.github.com <112259071+nakanosyouta@users.noreply.github.com>
2022-11-20 10:23:50 +0000

    Initial Commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to user_path(current_user)
+    end
+  end
+
+
+  def index
+    @book = Book.new
+    @users = User.all
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+      flash[:notice] = "You have updated user successfully." 
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+end
